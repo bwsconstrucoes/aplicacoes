5a2550dda564ebda8c99057eec2674cb61f9e0ec
Marcelo Leitão <contato@bwsconstrucoes.com.br>
2025-06-13 16:33:06 -0300

    Corrige host e porta para deploy Render

--- app.py
+++ app.py
@@ -96,4 +96,6 @@ def home():
     return 'API de Processamento de PDFs Online!'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
